b36fdc21009edd67b12fc8f064c4084c7dbf201d
thiagollipe-web <thiagollipe@gmail.com>
2026-09-15 23:08:19 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE="poda-v1";self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.add("./")));self.skipWaiting()});self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
